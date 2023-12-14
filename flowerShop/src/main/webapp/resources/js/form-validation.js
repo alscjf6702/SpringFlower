32649e01209839881fd690a35c90ad985929e302
Kim Minchul <alscjf6702@naver.com>
2023-12-14 12:55:28 +0900

    시작

--- flowerShop/src/main/webapp/resources/js/form-validation.js
+++ flowerShop/src/main/webapp/resources/js/form-validation.js
@@ -14,45 +14,51 @@ function check_Notice() {
         return true;
     }
 }
+/*------------------------약관 동의------------------------*/
+
+
+
 /*------------------------회원가입------------------------*/
 
 function check_Signup() {
-	if(document.frm.userId.value ===""){
-	        alert("아이디를 입력하세요.");
-	        document.frm.userId.focus();
-	        return false;
-	}
-	if(document.frm.pwd.value ===""){
-	         alert("비밀번호를 입력하세요.");
-	        document.frm.pwd.focus();
-	        return false;
-	}
-	if(document.frm.confirmPwd.value ===""){
-	         alert("비밀번호를 확인하세요.");
-	        document.frm.confirmPwd.focus();
-	        return false;
-	}
-	if(document.frm.userName.value ===""){
-	         alert("이름을 입력하세요.");
-	        document.frm.userName.focus();
-	        return false;
-	}
+   if(document.frm.userId.value ===""){
+           alert("아이디를 입력하세요.");
+           document.frm.userId.focus();
+           return false;
+   }
+   if(document.frm.pwd.value ===""){
+            alert("비밀번호를 입력하세요.");
+           document.frm.pwd.focus();
+           return false;
+   }
+   if(document.frm.confirmPwd.value ===""){
+            alert("비밀번호를 확인하세요.");
+           document.frm.confirmPwd.focus();
+           return false;
+   }
+   if(document.frm.userName.value ===""){
+            alert("이름을 입력하세요.");
+           document.frm.userName.focus();
+           return false;
+   }    return true;
+   
 }
-	
-	
+   
+   
 function check_ConfirmPwd() {
     var pwd = document.frm.pwd.value;
     var confirmPwd = document.frm.confirmPwd.value;
     var errorMsg_Pwd = document.getElementById("errorMsg_Pwd");
 
     if (pwd !== confirmPwd) {
-        errorMsg_Pwd.innerHTML = '<span style="color: red;">&nbsp;비밀번호가 일치하지 않습니다.</span>';
+        errorMsg_Pwd.innerHTML = '<span style="color: red;"> &nbsp; 비밀번호가 일치하지 않습니다. </span>';
     } else {
         errorMsg_Pwd.innerHTML = '';
     }
 }
 
-function updateHiddenField() {
+
+function check_Phone() {
     var phone1 = document.getElementById('phone_1').value;
     var phone2 = document.getElementById('phone_2').value;
     var phone3 = document.getElementById('phone_3').value;
@@ -61,13 +67,4 @@ function updateHiddenField() {
     document.getElementById('phone').value = phone;
 }
 
-        const phone = $("#phone");
-  $("#regibtn").on("click",function(){
-		const phone_1 =  $("#phone_1").val();
-		const phone_2 = $("#phone_2").val();
-		const phone_3 = $("#phone_3").val();
-        combinedPhone = phone_1+phone_2+phone_3;    
-
-    phone.val(combinedPhone);
-    });
-	
+/*------------------------rrrrr------------------------*/
